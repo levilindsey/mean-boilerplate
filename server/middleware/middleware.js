a6c88c756a1f6ccf732794f55f4de98f15845b9e
Levi Lindsey <levi@idean.com>
2014-07-11 09:52:19 -0700

    Adding some middleware.

--- server/middleware/middleware.js
+++ server/middleware/middleware.js
@@ -15,6 +15,10 @@ var config = require('../config/config');
 exports.init = function (server) {
   var morgan = require('morgan'), // For logging
       favicon = require('serve-favicon'), // For serving our favicon
+      bodyParser = require('body-parser'), // For parsing urlencoded and json request bodies
+      cookieParser = require('cookie-parser'),
+      session = require('express-session'),
+      MongoStore = require('connect-mongo')(session),
       staticFiles = require('./static-files');
 
   // Set up the templating engine
@@ -23,6 +27,15 @@ exports.init = function (server) {
 
   server.use(morgan({ format: 'dev', immediate: true }));
   server.use(favicon(config.faviconPath));
+  server.use(bodyParser());
+  server.use(cookieParser());
+  server.use(session({
+    secret: config.labs.sessionSecret,
+    store: new MongoStore({
+      mongoose_connection: db.getDatabaseConnection(),
+      collection: 'sessions'
+    })
+  }));
 
   staticFiles.init(server);
 };
